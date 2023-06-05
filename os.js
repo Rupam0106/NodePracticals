b4890da04f53a68bb608d248f5298302975f21d3
deepak_kumar <deepak.kumar@rudrainnovative.in>
2023-06-05 19:16:11 +0530

    Initial Commit

--- os.js
+++ os.js
@@ -1,4 +1,10 @@
 const os=require("os");
 
+console.log("Platform: " + os.platform());
+console.log("Architecture: " + os.arch());
 console.log(os.totalmem());
 console.log(os.freemem());
+console.log(os.uptime())
+console.log(os.type())
+console.log(os.release())
+console.log(os.hostname())
--- path.js
+++ path.js
@@ -3,6 +3,8 @@ const path=require("path");
 const res=path.parse(__dirname)
 const basename=path.basename(__dirname)
 console.log(path.delimiter)
+console.log(path.dirname(__dirname))
+
 
 console.log(basename)
 module.exports=res;

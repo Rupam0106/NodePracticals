48c6a476d63827f40ce11a954c21e0974d0640c8
rupam0106 <rupam.gupta@rudrainnovative.in>
2023-06-07 18:25:31 +0530

    Initial Commit

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,5 +1,4 @@
-const data=[]
-
+const data = [{ id: 1, name: "rupam" }];
 
 //test the endpoint
 exports.test = (req, res) => {
@@ -7,19 +6,57 @@ exports.test = (req, res) => {
 };
 
 // add the data
-exports.addData=(req,res)=>{
-    const {name}=req.body;
-     data.push(name);
-     res.status(201).json({
-        success:true,
-        message:"Name Added Successfully"
-     })
-}
+exports.addData = (req, res) => {
+  const { id, name } = req.body;
+  if(!id && !name){
+    return res.status(400).json({
+      success:false,
+      message:"Please provide name and Id"
+    })
+  }
+  data.push({ id, name });
+  res.status(201).json({
+    success: true,
+    message: "Name Added Successfully",
+  });
+};
 
 //get the all data
-exports.getData=(req,res)=>{
+exports.getData = (req, res) => {
+  res.status(201).json({
+    success: true,
+    data,
+  });
+};
+
+//update the data
+exports.updateData = (req, res) => {
+  const id=req.params.id;
+  if (!id) {
+    return res.status(400).json("Please Provide id");
+  }
+  const present = data.find((i) => i.id === id);
+  console.log(present)
+  if (!present) {
+    return res.status(400).json("User is not present ");
+  } else {
+    data.name = req.body.name;
     res.status(201).json({
-        success:true,
-        data
-     })
-}
+      success: true,
+      message: "User updated Successfully",
+    });
+  }
+};
+
+//delete the data
+exports.deleteData = (req, res) => {
+  const id = req.params.id;
+  if (!id) {
+    return res.status(400).json("Please Provide id");
+  }
+  data.filter((id) => id !== data.id);
+  res.status(201).json({
+    success: true,
+    message: "User Deleted Successfully",
+  });
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
 const express=require("express");
-const { test, addData, getData } = require("../controller/user");
+const { test, addData, getData, updateData, deleteData } = require("../controller/user");
 
 const router=express.Router();
 
@@ -9,6 +9,8 @@ router.get("/",(req,res)=>{
 router.route("/test").get(test)
 router.route("/add").post(addData)
 router.route("/get").get(getData)
+router.route("/update/:id").put(updateData)
+router.route("/delete/:id").delete(deleteData)
 
 router.get("*",(req,res)=>{
     res.status(200).json("Page not Found")
